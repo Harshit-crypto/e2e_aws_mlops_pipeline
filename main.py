ab1a79533ae74a3fe7253e092882b8b241acc9ac
Harshit-crypto <hjain.1523@gmail.com>
2025-08-02 17:34:32 +0530

    added model trainer

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from src.datascience_project import logger
 from src.datascience_project.pipelines.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.datascience_project.pipelines.data_validation_pipeline import DataValidationTrainingPipeline
-
+from src.datascience_project.pipelines.data_transformation_pipeline import DataTransformationTrainingPipeline
+from src.datascience_project.pipelines.model_trainer_pipeline import ModelTrainerTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -20,6 +21,27 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataValidationTrainingPipeline()
    data_ingestion.initiate_data_validation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.initiate_data_transformation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.initiate_model_training()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
